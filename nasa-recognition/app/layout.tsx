176937e6a690bd26518c68420bb0484732763f40
Wesley Kamau <weskam12312@gmail.com>
2025-12-11 23:35:54 -0500

    Add apple mobile web app title meta to RootLayout

--- nasa-recognition/app/layout.tsx
+++ nasa-recognition/app/layout.tsx
@@ -57,6 +57,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="apple-mobile-web-app-title" content="MSFC Faces" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
